cd5f64357428037ed34ebf4c845f5ac440386e24
George Norberg <george.norberg@gmail.com>
2017-08-16 14:10:51 -0700

    Move export syntax to function declaration

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -5,7 +5,7 @@ export default function Box({ board, boxStyle, coords, flip }) {
   const column = coords[2];
 
   return (
-    // Flip updates state in App
+    // Flip updates board state in App component
     <button className="box" style={boxStyle} onClick={() => flip(row, column)}>
       {board[row][column]}
     </button>
