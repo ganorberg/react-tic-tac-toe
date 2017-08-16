e6758799a45d0c9cc55fc988df9eaf241ca3f541
George Norberg <george.norberg@gmail.com>
2017-08-16 14:11:28 -0700

    Add diagonal check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Row from './Row.jsx'
 import StartButton from './StartButton.jsx'
 
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -10,15 +10,17 @@ class App extends Component {
               ['-', '-', '-'],
               ['-', '-', '-']],
       style: {
-        outline: "none",
-        fontSize: `${100}px`
+              outline: "none",
+              fontSize: `${100}px`
       },
     }
 
-    this.flip = this.flip.bind(this),
-    this.newGame = this.newGame.bind(this),
     this.checkWin = this.checkWin.bind(this),
     this.flashWin = this.flashWin.bind(this)
+    this.flip = this.flip.bind(this),
+    this.newGame = this.newGame.bind(this),
+    
+    // counter used to alternate X and O as well as determine draw
     this.counter = 0;
   }
 
@@ -33,61 +35,87 @@ class App extends Component {
     this.counter = 0;
   }
 
-  checkWin(row, column, lastMove) {
-    let middle = this.state.board[1][1];
-
+  checkWin(row, column, lastLetter) {
     // Check row and column last move resides in
-    for (let i = 0; i < 3; i += 1) {
-      if (this.state.board[row][i] !== lastMove) break;
-      if (i == 2) return this.flashWin(lastMove);
+    for (let testColumn = 0; testColumn < 3; testColumn += 1) {
+      if (this.state.board[row][testColumn] !== lastLetter) break;
+      if (testColumn === 2) {
+        this.flashWin(lastLetter);
+        return;
+      }
     }
     
-    for (let i = 0; i < 3; i += 1) {
-      if (this.state.board[i][column] !== lastMove) break;
-      if (i == 2) return this.flashWin(lastMove);
+    for (let testRow = 0; testRow < 3; testRow += 1) {
+      if (this.state.board[testRow][column] !== lastLetter) break;
+      if (testRow === 2) {
+        this.flashWin(lastLetter);
+        return;
+      }
     }
 
-    // Diagonal check. 
-    
-    if (this.counter > 8) this.flashWin('DRAW');
+    // Diagonal check
+    const topLeft = this.state.board[0][0];
+    const topRight = this.state.board[0][2];
+    const middle = this.state.board[1][1];
+    const bottomLeft = this.state.board[2][0];
+    const bottomRight = this.state.board[2][2];
+
+    if (topLeft === lastLetter &&
+        middle === lastLetter &&
+        bottomRight === lastLetter
+        ||
+        topRight === lastLetter &&
+        middle === lastLetter &&
+        bottomLeft === lastLetter) {
+      this.flashWin(lastLetter);
+      return;
+    }
+
+    if (this.counter > 8) { this.flashWin('DRAW'); }
   }
 
   flashWin(win) {
-    if (win === 'DRAW') {
-      this.setState(
-        {board: [[win, win, win],
-                 [win, win, win],
-                 [win, win, win]],
-         style: {fontSize: 50 + 'px'}
-        }
-      );
-    } else {
-      this.setState(
-        {board: [[win, win, win],
-                 ['W', 'I', 'N'],
-                 [win, win, win]]
-        }
-      );
+    const draw = {
+      board: [[win, win, win],
+              [win, win, win],
+              [win, win, win]],
+      style: {fontSize: 50 + 'px'}
     }
+
+    const winner = {
+      board: [[win, win, win],
+              ['W', 'I', 'N'],
+              [win, win, win]]
+    }
+
+    if (win === 'DRAW') { this.setState(draw); }
+    else { this.setState(winner); }
   }
 
   flip(row, column) {
-    if (this.state.board[row][column] === '-') {
-      let toChange;
-      this.counter % 2 ? toChange = 'X' : toChange = 'O'
-      this.counter += 1;
+    if (this.state.board[row][column] !== '-') { return; }
+   
+    const letter = this.counter % 2 ? 'X' : 'O';
+    this.counter++;
 
-      // Deep clone board to mutate values on clone. Then set state to mutated clone.
-      let newBoard = JSON.parse(JSON.stringify(this.state.board));
-      newBoard[row][column] = toChange;
-      this.setState({board: newBoard}, this.checkWin.bind(this, row, column, toChange));
-    }
+    // Clone board to avoid mutating state directly
+    const newBoard = Object.assign({}, this.state.board);
+    newBoard[row][column] = letter;
+
+    // Trigger callback to check win conditions every time board is updated
+    this.setState({ board: newBoard }, () => this.checkWin(row, column, letter));
   }
 
   render() {
-    let rows = [];
-    for (let i = 0; i < 3; i += 1) {
-      rows[i] = <Row board={this.state.board} boxStyle={this.state.style} row={i} key={i} flip={this.flip} />
+    const rows = [];
+    for (let row = 0; row < 3; row++) {
+      rows.push(<Row
+        board={this.state.board}
+        boxStyle={this.state.style}
+        flip={this.flip}
+        row={row}
+        key={row}
+      />);
     }
     
     return (
@@ -101,5 +129,3 @@ class App extends Component {
     )
   }
 }
-
-export default App
